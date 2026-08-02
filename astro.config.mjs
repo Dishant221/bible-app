0f18d9da23bf871b31370ea9e3f8e3cfe3d15371
dtotade <dishant.totade@e2open.com>
2026-08-02 07:29:56 +0530

    removeew cloudflare adapter and revert tp statisoutput

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from 'astro/config';
+   import sitemap from '@astrojs/sitemap';
+   import compress from 'astro-compress';
 
 export default defineConfig({
   site: 'https://biblestudyhub.com',
   output: 'hybrid',
-  adapter: cloudflare(),
+  //adapter: cloudflare(),
+  integrations: [tailwind(), sitemap(), compress()],
 
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     mdx(),
-    // sitemap(), // TODO: Fix sitemap generation issue
+    // sitemap(), // TODO: Fix `sitemap generation issue
     compress(),
   ],
 
@@ -71,3 +75,7 @@ export default defineConfig({
     prefetchAll: true,
   },
 });
+export default defineConfig({
+     output: 'static',
+     integrations: [sitemap(), compress()],  // keep whatever integrations you already had
+   });
